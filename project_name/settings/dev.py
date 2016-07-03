cba002f9827cfd14b7aa17958cbe903ee182f581
Alexander <a@burmystrov.com>
2016-07-03 22:18:59 +0300

    Removed explicit setup for django debug toolbar (#1)

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -6,18 +6,7 @@ from .base import *  # NOQA
 DEBUG = True
 
 # TOOLBAR CONFIGURATION
-# See: http://django-debug-toolbar.readthedocs.org/en/latest/installation.html#explicit-setup
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-# http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
-INTERNAL_IPS = ('127.0.0.1',)
+INSTALLED_APPS += ('debug_toolbar',)
 
 # EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
